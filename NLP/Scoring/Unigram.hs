6f3a0ea858e14ee8e31e7aea51a24616f53833df
Christian Hoener zu Siederdissen <choener@bioinf.uni-leipzig.de>
2017-03-29 20:56:29 +0200

    most of new parser in place
    now creating error captioning cases

---
+++ NLP/Scoring/Unigram.hs
@@ -0,0 +1,80 @@
+
+-- | This module defines a simple scoring scheme based on pairs of unigrams.
+
+module NLP.Scoring.Unigram where
+
+import GHC.Generics
+import Data.HashMap.Strict
+import Data.Aeson
+
+import NLP.Text.BTI
+
+
+
+-- | Score 'BTI's @x@ and @y@ based on the simple scoring system: (i)
+-- lookup (x,y) and use the score if found; (ii) if (x,y) is not in the
+-- database, then return the default matching 'defaultMatch' score if
+-- @x==y@, otherwise return the default mismatch 'defaultMismatch' score.
+
+scoreUnigram :: UnigramScoring -> BTI -> BTI -> Double
+scoreUnigram UnigramScoring {..} x y =
+  lookupDefault (if x==y then defaultMatch else defaultMismatch) (x,y) unigramMatch
+{-# Inline scoreUnigram #-}
+
+-- | Collect the hashtable and scalar values for simple scoring.
+--
+-- TODO binary and cereal instances
+
+data UnigramScoring = UnigramScoring
+  { unigramMatch          :: !(HashMap (BTI,BTI) Double)
+  -- ^ All known matching characters and associated scores.
+  , specialMismatch       :: !(HashMap BTI Double)
+  -- ^ Characters that can be deleted with costs different from
+  -- @gapOpen@/@gapExtension@.
+  , gapLinear             :: !Double
+  -- ^ linear gap scores
+  , gapOpen               :: !Double
+  -- ^ Gap opening costs for Gotoh-style grammars.
+  , gapExtension          :: !Double
+  -- ^ Gap extension costs for Gotoh-style grammars.
+  , defaultMatch          :: !Double
+  -- ^ Default score for characters matching, i.e. @x==y@.
+  , defaultMismatch       :: !Double
+  -- ^ Default score for characters not matching, i.e. @x/=y@.
+  , prefixSuffixLinear    :: !Double
+  -- ^ Special gap score for a prefix or suffix.
+  , prefixSuffixOpen      :: !Double
+  -- ^ Special gap opening score for a prefix or suffix.
+  , prefixSuffixExtension :: !Double
+  -- ^ Special gap extension score for a prefix or suffix.
+  }
+  deriving (Read,Show,Eq,Generic)
+
+instance FromJSON UnigramScoring where
+  parseJSON (Object v)
+    =   UnigramScoring
+    <$> (fromList `fmap` (v .: "simpleScore"))
+    <*> (fromList `fmap` (v .: "specialMismatch"))
+    <*> v .: "gapLinear"
+    <*> v .: "gapOpen"
+    <*> v .: "gapExtension"
+    <*> v .: "defaultMatch"
+    <*> v .: "defaultMismatch"
+    <*> v .: "prefixSuffixLinear"
+    <*> v .: "prefixSuffixOpen"
+    <*> v .: "prefixSuffixExtension"
+
+instance ToJSON UnigramScoring where
+  toJSON UnigramScoring {..}
+    = object [ "simpleScore"            .= toList unigramMatch
+             , "specialMismatch"        .= toList specialMismatch
+             , "gapLinear"              .= gapLinear
+             , "gapOpen"                .= gapOpen
+             , "gapExtension"           .= gapExtension
+             , "defaultMatch"           .= defaultMatch
+             , "defaultMismatch"        .= defaultMismatch
+             , "prefixsuffixLinear"     .= prefixSuffixLinear
+             , "prefixSuffixOpen"       .= prefixSuffixOpen
+             , "prefixSuffixExtension"  .= prefixSuffixExtension
+             ]
+
--- NLP/Scoring/Unigram/Import.hs
+++ NLP/Scoring/Unigram/Import.hs
@@ -1,19 +1,190 @@
 
-module NLP.Scoring.SimpleUnigram.Import where
+-- |
+--
+-- TODO normalization of characters!
+
+module NLP.Scoring.Unigram.Import where
 
 import           Control.Applicative
-import           Data.HashMap.Strict (fromList)
+import           Control.Lens
+import           Control.Monad
+import           Control.Monad.State.Class
+import           Control.Monad.Trans.State.Strict hiding (gets)
+import           Data.HashMap.Strict (fromList, HashMap)
+import           Data.HashSet (HashSet)
+import           Data.String (IsString)
 import           Data.Text (Text)
-import qualified Data.Attoparsec.Text as AT
+import           Debug.Trace
+import qualified Data.HashMap.Strict as HM
+import qualified Data.HashSet as HS
 import qualified Data.Text as T
 import qualified Data.Text.IO as T
-import           Data.Attoparsec.Text ((<?>))
+import           Text.Parser.LookAhead
+import           Text.Parser.Token.Style
+import           Text.Trifecta as TT
+import qualified Data.Sequence as S
+import           Data.Monoid
+import           Data.Char
 
 import           NLP.Text.BTI
 
-import           NLP.Scoring.SimpleUnigram
+import           NLP.Scoring.Unigram
 
 
+
+data Env = Env
+  { _equalChars     :: HashSet (Text, [Text])
+  , _similarChars   :: HashSet (Text, [Text])
+  , _warnings       :: S.Seq Text
+  , _equalScores    :: HashMap Text Double
+  , _similarScores  :: HashMap (Text,Text) Double
+  , _constants      :: HashMap Text Double
+  }
+  deriving (Show)
+
+makeLenses ''Env
+
+defaultEnv = Env
+  { _equalChars     = HS.empty
+  , _similarChars   = HS.empty
+  , _warnings       = S.empty
+  , _equalScores    = HM.empty
+  , _similarScores  = HM.empty
+  , _constants      = HM.empty
+  }
+
+
+
+test = fromFile "scoring/simpleunigram.score"
+
+fromFile :: FilePath -> IO (Maybe UnigramScoring)
+fromFile fp = do
+  p' <- TT.parseFromFile ((runStateT . runUnigramParser) pUnigram defaultEnv) fp
+  case p' of
+    Nothing    -> return Nothing
+    Just (p,e) -> do
+      let ws = e^.warnings
+      unless (null ws) $ do
+        mapM_ T.putStrLn ws
+      return $ Just p
+
+pUnigram :: UnigramParser UnigramScoring
+pUnigram = do
+  someSpace
+  many $ choice [pEqSet, pSet, pEqualScores, pSimilarScores, pConstants]
+  eof
+  let uconstants :: Text -> UnigramParser Double
+      uconstants k = do
+        kv <- use constants
+        case HM.lookup k kv of
+          Nothing -> do
+            warnings %= (S.|> ("constant " <> k <> " not found, using default (-999999)"))
+            return (-999999)
+          Just v  -> return v
+  -- now we create the list of all matches of characters (which includes
+  -- similar and dissimilar characters) and maximize over the possible
+  -- scores.
+  let equalchars = []
+  let unigramMatch = HM.fromListWith max $ equalchars
+  let specialMismatch = HM.empty
+  gapLinear     <- uconstants "Gap"
+  gapOpen       <- uconstants "GapOpen"
+  gapExtension  <- uconstants "GapExtension"
+  defaultMatch    <- uconstants "Match"
+  defaultMismatch <- uconstants "Mismatch"
+  prefixSuffixLinear    <- uconstants "PrefixSuffixLinear"
+  prefixSuffixOpen      <- uconstants "PrefixSuffixOpen"
+  prefixSuffixExtension <- uconstants "PrefixSuffixExtension"
+  -- Given the @Env@, we can now construct the actual scoring system.
+  return UnigramScoring{..}
+
+-- | Read a line containing an "EqSet". Will only parse successfully if the
+-- set is not yet known. Inserts the set into the @Env@.
+
+pEqSet :: UnigramParser ()
+pEqSet = do
+  ks <- uses equalScores HM.keys
+  reserve reserved "EqSet"
+  ty <- choice $ map textSymbol ks
+  vs <- runUnlined $ some pGrapheme
+  someSpace
+  equalChars %= (HS.insert (ty, vs))
+
+pSet :: UnigramParser ()
+pSet = do
+  reserve reserved "Set"
+  ty <- ident reserved
+  vs <- runUnlined $ some pGrapheme
+  someSpace
+  similarChars %= HS.insert (ty,vs)
+  return ()
+
+pEqualScores :: UnigramParser ()
+pEqualScores = do
+  reserve reserved "Eq"
+  ty <- ident reserved
+  v  <- either fromIntegral id <$> integerOrDouble
+  equalScores %= HM.insert ty v
+
+pSimilarScores :: UnigramParser ()
+pSimilarScores = do
+  reserve reserved "InSet"
+  ty1 <- ident reserved
+  ty2 <- ident reserved
+  v <- either fromIntegral id <$> integerOrDouble
+  similarScores %= HM.insert (ty1,ty2) v
+
+-- | Small parsers for the different constants we have.
+--
+-- TODO bail if we see a constant twice?
+
+pConstants :: UnigramParser ()
+pConstants = choice $ map pConstant cs
+  where
+    cs = [ "Gap", "GapOpen", "GapExtend", "PrefixSuffixOpen", "PrefixSuffixExtend"
+         , "Match", "Mismatch"
+         ]
+    pConstant :: Text -> UnigramParser ()
+    pConstant r = do
+      reserveText reserved r
+      v <- either fromIntegral id <$> integerOrDouble
+      constants %= HM.insert r v
+
+pGrapheme :: (CharParsing p, TokenParsing p) => p Text
+pGrapheme = T.pack <$> some (satisfy (not . isSpace)) <* someSpace
+
+-- | TODO only insert warning, not error, after seeing a character again!
+
+setIdent :: HashSet Text -> Unlined UnigramParser Text
+setIdent e = try $ do
+  k <- ident reserved
+  when (HS.member k e) $ fail "Character already present in EqSet!"
+  return k
+
+reserved :: TokenParsing m => IdentifierStyle m
+reserved = emptyIdents { _styleReserved = rs }
+  where rs = HS.fromList [ "EqSet", "Set", "Eq", "InSet"
+                         ]
+
+
+newtype UnigramParser a = UnigramParser { runUnigramParser :: StateT Env Parser a }
+  deriving
+    ( Alternative
+    , Applicative
+    , CharParsing
+    , Functor
+    , Monad
+    , MonadPlus
+    , MonadState Env
+    , Parsing
+    , LookAheadParsing
+    )
+
+instance TokenParsing UnigramParser where
+  someSpace = buildSomeSpaceParser (() <$ space) haskellCommentStyle
+
+
+{-
 
 -- | Each parsed line gives a set of characters, or tells us a score.
 --
@@ -104,4 +275,6 @@ genSimpleScoring l = SimpleScoring{..} -- SimpleScoring t g go ge dm di
 -- | parse a simple scoring file.
 
 simpleScoreFromFile f = T.readFile f >>= return . genSimpleScoring
+
+-}
 
